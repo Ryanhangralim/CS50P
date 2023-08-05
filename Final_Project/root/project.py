f040cc49032596d5f55743b9dcce5677c3fc7d4e
Ryanhangralim <120095775+Ryanhangralim@users.noreply.github.com>
2023-08-05 12:23:10 +0800

    CS50P Final Project Done

--- Final_Project/root/project.py
+++ Final_Project/root/project.py
@@ -1,17 +1,46 @@
+"""
+Project Title : YouTube to MP3 Downloader
+Name : Ryan Hangralim
+City : Denpasar, Bali
+Country : Indonesia
+"""
+
 import re
+from pyfiglet import Figlet
+from pytube import YouTube
+from pytube.exceptions import VideoUnavailable
+
+figlet = Figlet()
+
 
 def main():
-    link = "https://www.youtube.com/watch?v=fJ9rUzIMcZQ&pp=ygUFcXVlZW4%3D"
-    link = link.strip()
-    if(is_ValidLink(123)):
-        print("Valid")
-    else:
-        print("Invalid")
+    while True:
+        print("YouTube to MP3 Downloader Menu")
+        print("Link Input:")
+        print("[1] Single Link")
+        print("[2] Multiple Links")
+        print("[3] Exit")
+        choice = input("Choice: ")
+
+        match choice:
+            case "1":
+                single_File()
+            case "2":
+                multi_File()
+            case "3":
+                break
+            case _:
+                print("Invalid Choice")
+    figlet.setFont(font="big")
+    print(figlet.renderText("This Was CS50"))
 
 
 def is_ValidLink(link):
     try:
-        if re.search(r"^((?:https?:)?\/\/)?((?:www|m)\.)?((?:youtube(-nocookie)?\.com|youtu.be))(\/(?:[\w\-]+\?v=|embed\/|live\/|v\/)?)([\w\-]+)(\S+)?$", link):
+        if re.search(
+            r"^((?:https?:)?\/\/)?((?:www|m)\.)?((?:youtube(-nocookie)?\.com|youtu.be))(\/(?:[\w\-]+\?v=|embed\/|live\/|v\/)?)([\w\-]+)(\S+)?$",
+            link,
+        ):
             return True
         else:
             return False
@@ -19,21 +48,93 @@ def is_ValidLink(link):
         return False
 
 
-
 def is_ValidOutputName(output):
-    pass
+    try:
+        if re.search(r"^[a-z0-9\-_ ]+$", output, re.IGNORECASE):
+            return True
+        else:
+            return False
+    except BaseException:
+        return False
 
 
-def download_Audio(link):
-    pass
+def download_Audio(link, output_name=None):
+    try:
+        yt = YouTube(link)
+        stream = yt.streams.get_by_itag(251)
+        if not output_name == None:
+            stream.download(filename=f"{output_name}.mp3")
+            print("Audio Downloaded")
+        else:
+            stream.download(filename=f"{yt.title}.mp3")
+            print("Audio Downloaded")
+    except VideoUnavailable:
+        print(f"Video {link} is unavailable")
+
+
+def is_ValidFile(file):
+    try:
+        if re.search(r"([a-z0-9_\- ])+\.txt", file, re.IGNORECASE):
+            return True
+        else:
+            return False
+    except BaseException:
+        return False
 
 
 def single_File():
-    pass
+    try:
+        #getting video link from user input
+        while True:
+            link = input("Video Link: ")
+            if is_ValidLink(link):
+                break
+            else:
+                print("Invalid Link")
+
+        #getting output name from user input
+        while True:
+            output = input("Output name (0 for default): ")
+            if output == "0":
+                break
+            elif is_ValidOutputName(output):
+                break
+            else:
+                print("Invalid Output")
+
+        if output == "0":
+            output = None
+
+        download_Audio(link, output)
+
+    except BaseException:
+        print("Invalid")
+        pass
 
 
 def multi_File():
-    pass
+    while True:
+        try:
+            file_input = input("Text file(.txt) with youtube links: ").strip()
+            if file_input == "0":
+                break
+
+            # validate user input
+            if is_ValidFile(file_input):
+                with open(file_input, "r") as file:
+                    links = file.readlines()
+
+            # iterating through file and downloading audio files
+            for i in range(len(links)):
+                link = links[i].strip()
+                if is_ValidLink(link):
+                    download_Audio(link)
+                else:
+                    print("Invalid Link, Skipping to Next Link")
+                    continue
+            break
+        except FileNotFoundError:
+            print("File Not Found, Try Again! (0 to return)")
 
 
 if __name__ == "__main__":
--- Final_Project/root/test_project.py
+++ Final_Project/root/test_project.py
@@ -1,4 +1,4 @@
-from project import is_ValidLink
+from project import is_ValidLink, is_ValidFile, is_ValidOutputName
 import pytest
 
 def test_isValidLink():
@@ -8,5 +8,23 @@ def test_isValidLink():
     assert is_ValidLink("https://youtu.be/fJ9rUzIMcZQ") == True
     assert is_ValidLink("https://cs50.harvard.edu/") == False
     assert is_ValidLink("www.google.com") == False
-    with pytest(BaseException):
+    with pytest.raises(BaseException):
         assert is_ValidLink(123)
+
+def test_isValidFile():
+    assert is_ValidFile("input.txt") == True
+    assert is_ValidFile("playlist1.txt") == True
+    assert is_ValidFile("my_play-list.txt") == True
+    assert is_ValidFile("my playlist.txt") == True
+    assert is_ValidFile("playlist.png") == False
+    assert is_ValidFile("playlist") == False
+    with pytest.raises(BaseException):
+        assert is_ValidLink(123)
+
+def test_isValidOutput():
+    assert is_ValidOutputName("Bohemian Rhapsody - Queen") == True
+    assert is_ValidOutputName("Bohemian Rhapsody") == True
+    assert is_ValidOutputName("CS50") == True
+    assert is_ValidOutputName("CS,50") == False
+    with pytest.raises(BaseException):
+        assert is_ValidOutputName(123)
